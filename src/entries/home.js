8e0d3618922b320d9e071bde01e655a08139f1ee
Klupzor <anloges@hotmail.com>
2018-03-07 10:03:25 -0500

    Organizando el proyecto

--- src/entries/home.js
+++ src/entries/home.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import {render} from 'react-dom'
-import data from './src/api.json'
-import Playlist from './src/playlist/components/playlist';
+// import data from './src/api.json'
+import Home from '../pages/containers/home';
 
 
 const app = document.getElementById('app')
 // ReactDOM.render(que voy a renderizar, donde lo hare)
 // const ms = <h1>hola mundo!!</h1>
-render(<Playlist data={data} />, app)
+render(<Home />, app)
