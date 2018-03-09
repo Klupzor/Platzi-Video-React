335bf4a076d80628e9e75b33062c6ed47c408798
Klupzor <anloges@hotmail.com>
2018-03-09 16:51:35 -0500

    Agrgando estilos a Modal

--- src/pages/containers/home.js
+++ src/pages/containers/home.js
@@ -4,15 +4,25 @@ import HomeLayout from '../components/home-layout';
 import Categories from './../../categories/components/categories';
 import Related from './../components/related';
 import ModalContainer from './../../widgets/containers/modal';
+import Modal from './../../widgets/components/modal';
 
 class Home extends Component {
+    handleCloseModalClick = (event) => {
+        this.setState({
+            modalVisible: false
+        })
+    }
     render () {
         return (
             <HomeLayout>
                 <Related/>
                 <Categories categories={this.props.data.categories}/>
                 <ModalContainer>
-                    <h1>Esto es un portal</h1>
+                    <Modal
+                        handleClick={this.handleCloseModalClick}
+                    >
+                        <h1>Esto es un portal</h1>
+                    </Modal>
                 </ModalContainer>
             </HomeLayout>
         )
