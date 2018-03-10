fb0d2ec6a767160280ffd8b8d8a628a837cb69a3
Klupzor <anloges@hotmail.com>
2018-03-10 12:22:57 -0500

    Creando UI de formularios

---
+++ src/widgets/components/search.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './search.css'
+
+const Search = () => (
+    <form action="" className="Search">
+    <input type="text"
+    placeholder="Busca tu vídeo favorito"
+    className="Search-input"
+    />
+    </form>
+)
+
+export default Search
